06b0843575d4eb56ed6689ac19d8f7a0a3b2dccd
Avinash <Avinash@zopnote.com>
2023-06-10 11:37:07 +0530

    remove run db migrations on start script

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
+# echo "run db migration"
+# /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
